aabeb24a1dff1fde81dbe3a82690325e00ba7721
bassel-mousa <--get>
2021-10-04 23:23:11 +0300

    adding Some Constatnts For Notification Channel

--- app/src/main/java/com/bassel/myspotifyclone/util/Constants.kt
+++ app/src/main/java/com/bassel/myspotifyclone/util/Constants.kt
@@ -2,4 +2,8 @@ package com.bassel.myspotifyclone.util
 
 object Constants {
     const val SONG_COLLECTION = "songs"
+
+    const val  NOTIFICATION_CHANNEL_ID = "music"
+
+    const val NOTIFICATION_ID = 1
 }
